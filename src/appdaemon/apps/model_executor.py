45f98bb71db3746f2b75d527ccefba652bd3921c
Chris Lai <lcmchristopher@gmail.com>
2021-12-12 00:35:32 +0000

    Added state listener and model pickling

--- src/appdaemon/apps/model_executor.py
+++ src/appdaemon/apps/model_executor.py
@@ -1,13 +1,36 @@
 import appdaemon.plugins.hass.hassapi as hass
-from model_creator import configuration
+import configuration
+import pickle
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
 
 
 class ModelExecutor(hass.Hass):
     def initialize(self):
-        self.handle = self.listen_state(
-            self.state_handler, entity=configuration.sensors
-        )
+        self.handle = self.listen_state(self.state_handler)
+        self.act_model_set = self.load_models()
+        print(self.act_model_set)
+
+    def load_models(self):
+        act_model_set = {}
+        for act in configuration.actuators:
+            with open(
+                f"{configuration.home}/src/appdaemon/apps/model/{act}.pickle", "rb"
+            ) as pickle_file:
+                content = pickle.load(pickle_file)
+                act_model_set[act] = content
+        return act_model_set
 
     def state_handler(self, entity, attribute, old, new, kwargs):
-        print(self.handle)
-        print(entity)
+
+        # Get state of all sensors for model input
+        sensor_state = {}
+        for sensor in configuration.sensors:
+            sensor_state[sensor] = self.get_state(entity_id=sensor)
+        sensor_state = pd.DataFrame.from_dict([sensor_state])
+        print(sensor_state)
+
+        # Execute all models for sensor
+        for act, model in self.act_model_set.items():
+            print(model)
+            print(model.predict(sensor_state))
--- src/model_creator/src/model_creator/3_learning_model.py
+++ src/model_creator/src/model_creator/3_learning_model.py
@@ -10,6 +10,7 @@ from sklearn.impute import SimpleImputer
 import numpy as np
 import os
 from sklearn.metrics import accuracy_score
+import pickle
 
 # Generate feature and output vectors from act states.
 df_act_states = pd.read_csv(configuration.states_csv)
@@ -20,8 +21,8 @@ feature_list = list(df_act_states.columns)
 for output in output_list:
     feature_list.remove(output)
 
-for actuators in configuration.actuators:
-    df_act = df_act_states[df_act_states["entity_id"] == actuators]
+for actuator in configuration.actuators:
+    df_act = df_act_states[df_act_states["entity_id"] == actuator]
     output_vector = df_act["entity_id"] + df_act["state"]
     feature_vector = df_act[feature_list]
 
@@ -34,10 +35,7 @@ for actuators in configuration.actuators:
     )  # 2)
 
     model_tree = DecisionTreeClassifier(min_samples_split=20, random_state=99)
-    model_logs = LogisticRegression(random_state=99, max_iter=1000)
-
     model_tree.fit(X, y)
-    model_logs.fit(X, y)
 
     def visualize_tree(tree, feature_names):
         """Create tree png using graphviz.
@@ -47,21 +45,26 @@ for actuators in configuration.actuators:
         tree -- scikit-learn DecsisionTree.
         feature_names -- list of feature names.
         """
-        with open(f"{actuators}.dot", "w") as f:
+        with open(f"{actuator}.dot", "w") as f:
             export_graphviz(tree, out_file=f, feature_names=feature_names)
 
-        command = ["dot", "-Tpng", f"{actuators}.dot", "-o", f"{actuators}.png"]
+        command = ["dot", "-Tpng", f"{actuator}.dot", "-o", f"{actuator}.png"]
         try:
             subprocess.check_call(command)
-            os.remove(f"{actuators}.dot")
+            os.remove(f"{actuator}.dot")
         except:
-            exit("Could not run dot, ie graphviz, to " "produce visualization")
+            exit("Could not run dot, ie graphviz, to produce visualization")
 
     visualize_tree(model_tree, feature_list)
 
     # Get predictions of model
     y_tree_predictions = model_tree.predict(X_test)
-    y_logs_predictions = model_logs.predict(X_test)
     # Extract predictions for each output variable and calculate accuracy and f1 score
     print(accuracy_score(y_test, y_tree_predictions) * 100)
-    print(accuracy_score(y_test, y_logs_predictions) * 100)
+
+    # Save model to disk
+    filename = open(
+        f"{configuration.home}/src/model_creator/src/model_creator/model/{actuator}.pickle",
+        "wb",
+    )
+    pickle.dump(model_tree, filename)
