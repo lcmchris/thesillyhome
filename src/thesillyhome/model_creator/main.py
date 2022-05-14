31bb2c56e47b43c6a6419c5479f7b00e754fcce8
Chris Lai <lcmchristopher@gmail.com>
2022-05-14 01:16:24 +0100

    Fixing path

--- src/thesillyhome/model_creator/main.py
+++ src/thesillyhome/model_creator/main.py
@@ -1,12 +1,12 @@
-import read_config_json as tsh_config
-from parse_data import parse_data_from_db
-from learning_model import train_model
+import thesillyhome.model_creator.read_config_json as tsh_config
+from thesillyhome.model_creator.parse_data import parse_data_from_db
+from thesillyhome.model_creator.learning_model import train_model
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     actuators = tsh_config.actuators
     sensors = tsh_config.sensors
     model_name_version = tsh_config.model_name_version
 
-    parse_data_from_db(actuators,sensors)
-    train_model(actuators,model_name_version)
+    parse_data_from_db(actuators, sensors)
+    train_model(actuators, model_name_version)
